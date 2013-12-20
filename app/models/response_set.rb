003ecc2e0398597c341c18b4fa19aa7d1e791e4c
Christopher Homan <cmh@cs.rit.edu>
2013-12-20 17:54:33 -0500

    Deleted unnecessary file

--- app/models/response_set.rb
+++
@@ -1,7 +0,0 @@
-class ResponseSet < ActiveRecord::Base
-  has_one :facebook_response_set
-
-  include Surveyor::Models::ResponseSetMethods
-  #include SurveyorTweaks::Models::ResponseSetMethods
-
-end
