b9e6ebef9bdceb7da01b01fcd8f1d225b6fd07bd
tripperroc <root@Christophers-MacBook-Pro.local>
2015-10-27 13:40:30 -0400

    Fixed answers for latino question; added some preliminary form validation to screening questions.

--- config.old/environments/productionx.rb
+++ config.old/environments/productionx.rb
@@ -77,15 +77,4 @@ GMHS::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.action_mailer.smtp_settings = {
-   :address => 'mailhost.cs.rit.edu',
-   :port => 587,
-   :domain => 'cs.rit.edu',
-   :user_name => 'cmh',
-   :password =>  'L4akewOod',
-   :authentication => 'plain',
-   :enable_starttls_auto => true
-  }
-
 end
